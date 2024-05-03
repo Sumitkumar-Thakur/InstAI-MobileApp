68566bbc4e3da71a3ae3a83e95ca6d82ada73e30
sumit <sumitashwanithakur2002@gmail.com>
2024-05-03 21:22:14 +0530

    adding routes

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
@@ -6,6 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl">aora!</Text>
       <StatusBar style="auto" />
+      <Link href="/home">Go to Home</Link>
     </View>
   );
 }
